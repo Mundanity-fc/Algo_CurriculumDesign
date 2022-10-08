15defb0413d2eb76011dd34e5e3f6230f071a61b
Mundanity-fc <i@mundanity.moe>
2022-10-08 20:21:26 +0800

    完成数独搜索回溯算法

--- Sudoku.h
+++ Sudoku.h
@@ -1,8 +1,3 @@
-#ifndef SUDOKU_SUDOKU_H
-#define SUDOKU_SUDOKU_H
-
-#endif //SUDOKU_SUDOKU_H
-
 #include <vector>
 #include <set>
 
@@ -33,6 +28,15 @@ private:
     set<int> sudokuSet;
     // 数独棋盘
     vector<vector<int>> map;
+    // 结果数组
+    vector<vector<vector<int>>> result;
+    // 全局计数器
+    int count;
+
+    // 从数集num中去除该行、列、块下所有已有数字
+    void deduplication(int rowNum, int colNum, set<int> &num);
+
+    void SudokuOut(vector<vector<int>> outMap);
 
 public:
     Sudoku() {
@@ -41,13 +45,17 @@ public:
         this->sudokuSet = set<int>(numList, numList + 9);
         // 数独棋盘初始化
         this->map = vector<vector<int>>(9, vector<int>(9));
+        this->count = 0;
     }
 
+    // 加载数独内容
     void initialMap(int initialMap[9][9]);
 
+    // 输出数独内容
     void printMap();
 
-    void solveQuiz();
+    // 搜索回溯
+    bool search();
 
     // 全局冲突检查，调用行、列、块冲突检查实现
     bool checkConflict();
@@ -60,10 +68,37 @@ public:
 
     // 块冲突检查
     bool checkBlockConflict(int rowNum, int colNum);
+
+    // 数独完成检测
+    bool checkFinish();
+
+    // 结果输出
+    void output();
 };
 
-bool Sudoku::checkConflict() {
+void Sudoku::initialMap(int initialMap[9][9]) {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            this->map[i][j] = initialMap[i][j];
+        }
+    }
+}
 
+bool Sudoku::checkConflict() {
+    for (int i = 0; i < 9; ++i) {
+        if (this->checkRowConflict(i))
+            return true;
+        if (this->checkColConflict(i))
+            return true;
+        if (i % 3 == 0) {
+            if (this->checkBlockConflict(i, 0))
+                return true;
+            if (this->checkBlockConflict(i, 3))
+                return true;
+            if (this->checkBlockConflict(i, 6))
+                return true;
+        }
+    }
     return false;
 }
 
@@ -132,49 +167,131 @@ bool Sudoku::checkBlockConflict(int rowNum, int colNum) {
     return false;
 }
 
-void Sudoku::initialMap(int initialMap[9][9]) {
-    for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            this->map[i][j] = initialMap[i][j];
-        }
-    }
+bool Sudoku::checkFinish() {
+    for (int i = 0; i < 9; ++i)
+        for (int j = 0; j < 9; ++j)
+            if (this->map[i][j] == 0)
+                return false;
+    return true;
+}
+
+void Sudoku::SudokuOut(vector<vector<int>> outMap) {
+    cout << "┌─────────┬─────────┬─────────┐\n";
+    cout << "│ " << outMap[0][0] << "  " << outMap[0][1] << "  " << outMap[0][2] << " │ ";
+    cout << outMap[0][3] << "  " << outMap[0][4] << "  " << outMap[0][5] << " │ ";
+    cout << outMap[0][6] << "  " << outMap[0][7] << "  " << outMap[0][8] << " " << "│\n";
+    cout << "│ " << outMap[1][0] << "  " << outMap[1][1] << "  " << outMap[1][2] << " │ ";
+    cout << outMap[1][3] << "  " << outMap[1][4] << "  " << outMap[1][5] << " │ ";
+    cout << outMap[1][6] << "  " << outMap[1][7] << "  " << outMap[1][8] << " " << "│\n";
+    cout << "│ " << outMap[2][0] << "  " << outMap[2][1] << "  " << outMap[2][2] << " │ ";
+    cout << outMap[2][3] << "  " << outMap[0][4] << "  " << outMap[2][5] << " │ ";
+    cout << outMap[2][6] << "  " << outMap[0][7] << "  " << outMap[2][8] << " " << "│\n";
+    cout << "├─────────┼─────────┼─────────┤\n";
+    cout << "│ " << outMap[3][0] << "  " << outMap[3][1] << "  " << outMap[3][2] << " │ ";
+    cout << outMap[3][3] << "  " << outMap[3][4] << "  " << outMap[3][5] << " │ ";
+    cout << outMap[3][6] << "  " << outMap[3][7] << "  " << outMap[3][8] << " " << "│\n";
+    cout << "│ " << outMap[4][0] << "  " << outMap[4][1] << "  " << outMap[4][2] << " │ ";
+    cout << outMap[4][3] << "  " << outMap[4][4] << "  " << outMap[4][5] << " │ ";
+    cout << outMap[4][6] << "  " << outMap[4][7] << "  " << outMap[4][8] << " " << "│\n";
+    cout << "│ " << outMap[5][0] << "  " << outMap[5][1] << "  " << outMap[5][2] << " │ ";
+    cout << outMap[5][3] << "  " << outMap[5][4] << "  " << outMap[5][5] << " │ ";
+    cout << outMap[5][6] << "  " << outMap[5][7] << "  " << outMap[5][8] << " " << "│\n";
+    cout << "├─────────┼─────────┼─────────┤\n";
+    cout << "│ " << outMap[6][0] << "  " << outMap[6][1] << "  " << outMap[6][2] << " │ ";
+    cout << outMap[6][3] << "  " << outMap[6][4] << "  " << outMap[6][5] << " │ ";
+    cout << outMap[6][6] << "  " << outMap[6][7] << "  " << outMap[6][8] << " " << "│\n";
+    cout << "│ " << outMap[7][0] << "  " << outMap[7][1] << "  " << outMap[7][2] << " │ ";
+    cout << outMap[7][3] << "  " << outMap[7][4] << "  " << outMap[7][5] << " │ ";
+    cout << outMap[7][6] << "  " << outMap[7][7] << "  " << outMap[7][8] << " " << "│\n";
+    cout << "│ " << outMap[8][0] << "  " << outMap[8][1] << "  " << outMap[8][2] << " │ ";
+    cout << outMap[8][3] << "  " << outMap[8][4] << "  " << outMap[8][5] << " │ ";
+    cout << outMap[8][6] << "  " << outMap[8][7] << "  " << outMap[8][8] << " " << "│\n";
+    cout << "└─────────┴─────────┴─────────┘\n";
 }
 
 void Sudoku::printMap() {
     cout << "目前数独的状态如下：\n";
-    cout << "┌─────────┬─────────┬─────────┐\n";
-    cout << "│ " << this->map[0][0] << "  " << this->map[0][1] << "  " << this->map[0][2] << " │ ";
-    cout << this->map[0][3] << "  " << this->map[0][4] << "  " << this->map[0][5] << " │ ";
-    cout << this->map[0][6] << "  " << this->map[0][7] << "  " << this->map[0][8] << " " << "│\n";
-    cout << "│ " << this->map[1][0] << "  " << this->map[1][1] << "  " << this->map[1][2] << " │ ";
-    cout << this->map[1][3] << "  " << this->map[1][4] << "  " << this->map[1][5] << " │ ";
-    cout << this->map[1][6] << "  " << this->map[1][7] << "  " << this->map[1][8] << " " << "│\n";
-    cout << "│ " << this->map[2][0] << "  " << this->map[2][1] << "  " << this->map[2][2] << " │ ";
-    cout << this->map[2][3] << "  " << this->map[0][4] << "  " << this->map[2][5] << " │ ";
-    cout << this->map[2][6] << "  " << this->map[0][7] << "  " << this->map[2][8] << " " << "│\n";
-    cout << "├─────────┼─────────┼─────────┤\n";
-    cout << "│ " << this->map[3][0] << "  " << this->map[3][1] << "  " << this->map[3][2] << " │ ";
-    cout << this->map[3][3] << "  " << this->map[3][4] << "  " << this->map[3][5] << " │ ";
-    cout << this->map[3][6] << "  " << this->map[3][7] << "  " << this->map[3][8] << " " << "│\n";
-    cout << "│ " << this->map[4][0] << "  " << this->map[4][1] << "  " << this->map[4][2] << " │ ";
-    cout << this->map[4][3] << "  " << this->map[4][4] << "  " << this->map[4][5] << " │ ";
-    cout << this->map[4][6] << "  " << this->map[4][7] << "  " << this->map[4][8] << " " << "│\n";
-    cout << "│ " << this->map[5][0] << "  " << this->map[5][1] << "  " << this->map[5][2] << " │ ";
-    cout << this->map[5][3] << "  " << this->map[5][4] << "  " << this->map[5][5] << " │ ";
-    cout << this->map[5][6] << "  " << this->map[5][7] << "  " << this->map[5][8] << " " << "│\n";
-    cout << "├─────────┼─────────┼─────────┤\n";
-    cout << "│ " << this->map[6][0] << "  " << this->map[6][1] << "  " << this->map[6][2] << " │ ";
-    cout << this->map[6][3] << "  " << this->map[6][4] << "  " << this->map[6][5] << " │ ";
-    cout << this->map[6][6] << "  " << this->map[6][7] << "  " << this->map[6][8] << " " << "│\n";
-    cout << "│ " << this->map[7][0] << "  " << this->map[7][1] << "  " << this->map[7][2] << " │ ";
-    cout << this->map[7][3] << "  " << this->map[7][4] << "  " << this->map[7][5] << " │ ";
-    cout << this->map[7][6] << "  " << this->map[7][7] << "  " << this->map[7][8] << " " << "│\n";
-    cout << "│ " << this->map[8][0] << "  " << this->map[8][1] << "  " << this->map[8][2] << " │ ";
-    cout << this->map[8][3] << "  " << this->map[8][4] << "  " << this->map[8][5] << " │ ";
-    cout << this->map[8][6] << "  " << this->map[8][7] << "  " << this->map[8][8] << " " << "│\n";
-    cout << "└─────────┴─────────┴─────────┘\n";
+    this->SudokuOut(this->map);
 }
 
-void Sudoku::solveQuiz() {
+bool Sudoku::search() {
+    cout << "第" << ++this->count << "次执行search\n";
+    // 每次开始前进行完成度检测，避免无效遍历
+    if (this->checkFinish()) {
+        // 数独完成且无冲突情况，记录为正确结果，进行结果输出并保存
+        if (not this->checkConflict()) {
+            cout << "发现一个结果\n";
+            this->result.push_back(this->map);
+            this->printMap();
+            /*
+             * cout << "键入回车以继续：";
+             * getchar();
+             */
+        }
+        // 数独完成时，该次搜索不进行，直接返回上层递归
+        return false;
+    }
 
+    // 搜索主体
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            // 跳过已有数字，寻找上层遍历进行后，数独中存在的下一个空位
+            if (this->map[i][j] != 0)
+                continue;
+            // 数字为 0 时，表示为空格，开始建立搜索环境
+            set<int> num = this->sudokuSet;
+            // 从数集中去除该行、列、块下所有已有数字
+            this->deduplication(i, j, num);
+            // 递归执行所有可能性
+            while (!num.empty()) {
+                // 取出集合中第一个元素
+                this->map[i][j] = *num.begin();
+                num.erase(num.begin());
+                this->search();
+            }
+            // 搜索结束返回上层时，需要将本层搜索结果清除
+            this->map[i][j] = 0;
+            return false;
+        }
+    }
+    return false;
+}
+
+void Sudoku::deduplication(int rowNum, int colNum, set<int> &num) {
+    vector<int> rowArray(3), colArray(3);
+    if (rowNum <= 2)
+        rowArray = {0, 1, 2};
+    else if (rowNum <= 5)
+        rowArray = {3, 4, 5};
+    else
+        rowArray = {6, 7, 8};
+
+    if (colNum <= 2)
+        colArray = {0, 1, 2};
+    else if (colNum <= 5)
+        colArray = {3, 4, 5};
+    else
+        colArray = {6, 7, 8};
+
+    // 去除行列内重复元素
+    for (int i = 0; i < 9; ++i) {
+        if (num.count(this->map[rowNum][i]))
+            num.erase(this->map[rowNum][i]);
+        if (num.count(this->map[i][colNum]))
+            num.erase(this->map[i][colNum]);
+    }
+
+    // 去除块内重复元素
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            if (num.count(this->map[rowArray[i]][colArray[j]]))
+                num.erase(this->map[rowArray[i]][colArray[j]]);
+}
+
+void Sudoku::output() {
+    cout << "共找到 " << this->result.size() << " 个结果，结果如下：\n";
+    for (auto &i: this->result) {
+        this->SudokuOut(i);
+        cout << endl;
+    }
 }
