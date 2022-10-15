d913312a6adec054611237e482832605947e6bc5
Mundanity-fc <i@mundanity.moe>
2022-10-15 22:30:27 +0800

    修改

--- Algo.py
+++ Algo.py
@@ -1,6 +1,11 @@
 from Louvain import Louvain
 
-if __name__ == "__main__":
+
+def SNAP():
     lou = Louvain('nodelist.txt')
     lou.getLabel('labellist.txt')
     lou.run()
+
+
+if __name__ == "__main__":
+    SNAP()
